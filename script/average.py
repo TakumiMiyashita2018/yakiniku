2b53c1ca50bd4c23c240d98c0ca9cff668ddc24c
TakumiMiyashita2018 <takumi.miyashita@syssec.cs.tsukuba.ac.jp>
2018-08-02 20:15:15 +0900

    作ったもの

---
+++ script/average.py
@@ -0,0 +1,55 @@
+import sys
+import tool
+import json
+
+file_name = sys.argv
+n_benign = n_maricious = n_unlabeled = 0
+l_benign = []
+l_maricious = []
+
+with open(file_name[1], "r") as file:
+    with open(file_name[2], "w") as sample:
+        line = file.readline()
+        while line:
+            f_tmp = json.loads(line)
+            l_tmp = tool.normalization(f_tmp)
+            if f_tmp["label"] == 0:
+                if l_benign:
+                    for i in range(len(f_tmp["histogram"])):
+                        l_benign[i] += l_tmp[i]
+                else:
+                    l_benign += l_tmp
+                n_benign += 1
+            elif f_tmp["label"] == 1:
+                if l_maricious:
+                    for i in range(len(f_tmp["histogram"])):
+                        l_maricious[i] += l_tmp[i]
+                else:
+                    l_maricious += l_tmp
+                n_maricious += 1
+            else:
+                n_unlabeled += 1
+            json.dump({"normalized": l_tmp, "label": f_tmp["label"]}, sample)
+            sample.write("\n")
+            line = file.readline()
+
+for i in range(len(l_benign)):
+    l_benign[i] /= n_benign
+for i in range(len(l_maricious)):
+    l_maricious[i] /= n_maricious
+
+benign = {
+        "average": l_benign,
+        "label": 0,
+        "number": n_benign
+}
+maricious = {
+        "average": l_maricious,
+        "label": 1,
+        "number": n_maricious
+}
+
+with open(file_name[3], "w") as file:
+    json.dump(benign, file)
+    file.write("\n")
+    json.dump(maricious, file)
---
+++ script/tool.py
@@ -0,0 +1,28 @@
+def normalization(dict):
+    sum = 0.0
+    list = dict["histogram"]
+
+    for num in list:
+        sum += num
+
+    for i in range(len(list)):
+        list[i] /= sum
+
+    return list
+
+def variance():
+    pass
+
+def compare(dict, l_test):
+    s_benign = s_maricious = 0
+
+    for i in range(256):
+        score = ((dict["a_b"][i] - l_test[i]) ** 2) * dict["w_b"][i]
+        s_benign += score
+        score = ((dict["a_m"][i] - l_test[i]) ** 2) * dict["w_m"][i]
+        s_maricious += score
+
+    if s_benign > s_maricious:
+        return False
+    else:
+        return True
