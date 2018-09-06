3869a04f3c95d25b167295b242db12e2d0852cff
TakumiMiyashita2018 <takumi.miyashita@syssec.cs.tsukuba.ac.jp>
2018-09-06 16:02:44 +0900

    fix average.py

--- script/average.py
+++ script/average.py
@@ -7,31 +7,32 @@ n_benign = n_maricious = n_unlabeled = 0
 l_benign = []
 l_maricious = []
 
-with open(file_name[1], "r") as file:
-    with open(file_name[2], "w") as sample:
-        line = file.readline()
-        while line:
-            f_tmp = json.loads(line)
-            l_tmp = tool.normalization(f_tmp)
-            if f_tmp["label"] == 0:
-                if l_benign:
-                    for i in range(len(f_tmp["histogram"])):
-                        l_benign[i] += l_tmp[i]
-                else:
-                    l_benign += l_tmp
-                n_benign += 1
-            elif f_tmp["label"] == 1:
-                if l_maricious:
-                    for i in range(len(f_tmp["histogram"])):
-                        l_maricious[i] += l_tmp[i]
-                else:
-                    l_maricious += l_tmp
-                n_maricious += 1
-            else:
-                n_unlabeled += 1
-            json.dump({"normalized": l_tmp, "label": f_tmp["label"]}, sample)
-            sample.write("\n")
+with open(file_name[1], 'w') as sample:
+    for i in range(3, len(file_name)):
+        with open(file_name[i], 'r') as file:
             line = file.readline()
+            while line:
+                f_tmp = json.loads(line)
+                l_tmp = tool.normalization(f_tmp)
+                if f_tmp["label"] == 0:
+                    if l_benign:
+                        for i in range(len(f_tmp["histogram"])):
+                            l_benign[i] += l_tmp[i]
+                    else:
+                        l_benign += l_tmp
+                    n_benign += 1
+                elif f_tmp["label"] == 1:
+                    if l_maricious:
+                        for i in range(len(f_tmp["histogram"])):
+                            l_maricious[i] += l_tmp[i]
+                    else:
+                        l_maricious += l_tmp
+                    n_maricious += 1
+                else:
+                    n_unlabeled += 1
+                json.dump({"normalized": l_tmp, "label": f_tmp["label"]}, sample)
+                sample.write("\n")
+                line = file.readline()
 
 for i in range(len(l_benign)):
     l_benign[i] /= n_benign
@@ -49,7 +50,7 @@ maricious = {
         "number": n_maricious
 }
 
-with open(file_name[3], "w") as file:
+with open(file_name[2], "w") as file:
     json.dump(benign, file)
     file.write("\n")
     json.dump(maricious, file)
