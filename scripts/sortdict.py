92e311bab9935badb6a6b0653189c137544f9f79
TakumiMiyashita2018 <takumi.miyashita@syssec.cs.tsukuba.ac.jp>
2018-09-22 16:32:39 +0900

    fix minor

--- scripts/sortdict.py
+++ scripts/sortdict.py
@@ -18,8 +18,10 @@ with open(file_name[2], 'w') as w:
     for i in range(10):
         w.write(f_maricious[i] + '\n')
 
+#print('benign: \n')
 #for i in range(10):
 #    print(f_benign[i])
 
+#print('benign: \n')
 #for i in range(10):
 #    print(f_maricious[i])
