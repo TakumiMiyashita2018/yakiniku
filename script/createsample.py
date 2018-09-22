89eebe6d0b57526f7c3d8a5e480599ec336b3465
TakumiMiyashita2018 <takumi.miyashita@syssec.cs.tsukuba.ac.jp>
2018-09-22 16:10:08 +0900

    iroiro

--- script/createsample.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-file_name = sys.argv
-
-with open(file_name[1], "r") as file:
-    with open(file_name[2], "w") as sample:
-        for i in range(200):
-            line = file.readline()
-            sample.write(line)
